94ff23ea6b9d0984f5e4eacecfdafe6cccb66d90
Samat <kosbarmak3@gmail.com>
2024-09-25 15:35:13 +0500

    added negative test genderNotSelectedTest and lastNameNotFilledTest

--- src/test/java/tests/FormAutomationTests.java
+++ src/test/java/tests/FormAutomationTests.java
@@ -3,8 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 import pages.FormAutomationPage;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FormAutomationTests extends BaseTestSetup {
@@ -48,5 +47,46 @@ public class FormAutomationTests extends BaseTestSetup {
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    }
+
+    // Негативный тест: Поле Gender не выбрано
+    @Test
+    void genderNotSelectedTest() {
+        formPage.openFormPage()
+                .fillFirstName("Sam")
+                .fillLastName("Baker")
+                .fillEmail("dsn@smda.la")
+                .fillPhoneNumber("7077470510")
+                .setBirthDate("October", "1995", "10")
+                .setSubject("Hindi")
+                .selectHobby()
+                .uploadPicture("image.png")
+                .setAddress("Sports street 43")
+                .selectStateAndCity("Uttar Pradesh", "Lucknow")
+                .submitForm();
+
+        // Проверка, что форма не отправилась, т.к. "Gender" не был выбран
+        $(".modal-dialog").shouldNot(appear); // Модальное окно не должно появить
+    }
+
+    // Негативный тест: Поле Last Name не заполнено
+    @Test
+    void lastNameNotFilledTest() {
+        formPage.openFormPage()
+                .fillFirstName("Sam")
+                // Пропускаем поле Last Name
+                .fillEmail("dsn@smda.la")
+                .selectGender("Male")
+                .fillPhoneNumber("7077470510")
+                .setBirthDate("October", "1995", "10")
+                .setSubject("Hindi")
+                .selectHobby()
+                .uploadPicture("image.png")
+                .setAddress("Sports street 43")
+                .selectStateAndCity("Uttar Pradesh", "Lucknow")
+                .submitForm();
+
+        // Проверка, что поле Last Name не заполнено, и форма не отправляется
+        $("#lastName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
     }
 }
