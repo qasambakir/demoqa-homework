02972fb978e63dfb4857daa6ff4e18ea70ee062f
Samat <kosbarmak3@gmail.com>
2024-09-25 21:39:49 +0500

    исправления замечаний по негативному тесту

--- src/test/java/pages/FormAutomationPage.java
+++ src/test/java/pages/FormAutomationPage.java
@@ -3,13 +3,14 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 import pages.components.DateSelectorComponent;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FormAutomationPage {
 
-    private final SelenideElement firstNameInput = $("#firstName"),
+    private final SelenideElement
+            firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
             userEmailInput = $("#userEmail"),
             genderWrapper = $("#genterWrapper"),
@@ -25,7 +26,6 @@ public class FormAutomationPage {
 
     final DateSelectorComponent dateSelector = new DateSelectorComponent();
 
-    // Открытие страницы формы
     public FormAutomationPage openFormPage() {
         open("/automation-practice-form");
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
@@ -34,68 +34,57 @@ public class FormAutomationPage {
         return this;
     }
 
-    // Заполнение имени
     public FormAutomationPage fillFirstName(String value) {
         firstNameInput.setValue(value);
         return this;
     }
 
-    // Заполнение фамилии
     public FormAutomationPage fillLastName(String value) {
         lastNameInput.setValue(value);
         return this;
     }
 
-    // Заполнение email
     public FormAutomationPage fillEmail(String value) {
         userEmailInput.setValue(value);
         return this;
     }
 
-    // Выбор пола
     public FormAutomationPage selectGender(String gender) {
         genderWrapper.$(byText(gender)).click();
         return this;
     }
 
-    // Заполнение номера телефона
     public FormAutomationPage fillPhoneNumber(String phoneNumber) {
         userNumberInput.setValue(phoneNumber);
         return this;
     }
 
-    // Установка даты рождения
     public FormAutomationPage setBirthDate(String month, String year, String day) {
-        dateInput.click(); // Открыть календарь
-        dateSelector.setDate(month, year, day); // Выбрать дату через компонент DateSelector
+        dateInput.click();
+        dateSelector.setDate(month, year, day);
         return this;
     }
 
-    // Установка предмета
     public FormAutomationPage setSubject(String subject) {
         subjectInput.setValue(subject).pressEnter();
         return this;
     }
 
-    // Выбор хобби
     public FormAutomationPage selectHobby() {
         hobbyCheckbox.click();
         return this;
     }
 
-    // Загрузка изображения
     public FormAutomationPage uploadPicture(String filePath) {
         uploadPictureInput.uploadFromClasspath(filePath);
         return this;
     }
 
-    // Заполнение адреса
     public FormAutomationPage setAddress(String address) {
         addressInput.setValue(address);
         return this;
     }
 
-    // Выбор штата и города
     public FormAutomationPage selectStateAndCity(String state, String city) {
         stateDropdown.click();
         $(byText(state)).click();
@@ -104,13 +93,16 @@ public class FormAutomationPage {
         return this;
     }
 
-    // Отправка формы
     public FormAutomationPage submitForm() {
         submitButton.click();
         return this;
     }
 
-    // Проверка результата формы
+    public FormAutomationPage checkFormNotSubmitted() {
+        $(".modal-dialog").shouldNot(appear);
+        return this;
+    }
+
     public FormAutomationPage checkFormResult(String key, String value) {
         $(".table-responsive").$(byText(key)).parent().shouldHave(text(value));
         return this;
--- src/test/java/tests/FormAutomationTests.java
+++ src/test/java/tests/FormAutomationTests.java
@@ -8,7 +8,6 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class FormAutomationTests extends TestBase {
 
-    // Создаём объект страницы формы
     final FormAutomationPage formPage = new FormAutomationPage();
 
     @Test
@@ -27,6 +26,7 @@ public class FormAutomationTests extends TestBase {
                 .selectStateAndCity("Uttar Pradesh", "Lucknow")
                 .submitForm();
 
+        // Проверка результатов после успешной отправки
         formPage.checkFormResult("Student Name", "Sam Baker")
                 .checkFormResult("Student Email", "qabaker@gmail.com")
                 .checkFormResult("Mobile", "7077570510")
@@ -38,6 +38,7 @@ public class FormAutomationTests extends TestBase {
                 .checkFormResult("Picture", "image.png");
     }
 
+    // Минимальная отправка формы
     @Test
     void minimalFormSubmissionTest() {
         formPage.openFormPage()
@@ -47,53 +48,26 @@ public class FormAutomationTests extends TestBase {
                 .fillPhoneNumber("9876543210")
                 .submitForm();
 
-        // Проверка, что форма отправилась, и значения были заполнены корректно
+        // Проверка успешной отправки
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
 
         formPage.checkFormResult("Student Name", "Test Testov")
                 .checkFormResult("Mobile", "9876543210")
-                .checkFormResult("Gender", "Male"); // Добавляем проверку заполненных полей
+                .checkFormResult("Gender", "Male");
     }
 
-    // Негативный тест: Поле Gender не выбрано
     @Test
-    void genderNotSelectedTest() {
+    public void formNotSubmittedWithoutRequiredFieldsTest() {
         formPage.openFormPage()
                 .fillFirstName("Sam")
-                .fillLastName("Baker")
                 .fillEmail("dsn@smda.la")
                 .fillPhoneNumber("7077470510")
                 .setBirthDate("October", "1995", "10")
-                .setSubject("Hindi")
-                .selectHobby()
-                .uploadPicture("image.png")
-                .setAddress("Sports street 43")
-                .selectStateAndCity("Uttar Pradesh", "Lucknow")
                 .submitForm();
 
         // Проверка, что форма не отправляется
         $(".modal-dialog").shouldNot(appear);
-    }
-
-    // Негативный тест: Поле Last Name не заполнено
-    @Test
-    void lastNameNotFilledTest() {
-        formPage.openFormPage()
-                .fillFirstName("Sam")
-                // Пропускаем поле Last Name
-                .fillEmail("dsn@smda.la")
-                .selectGender("Male")
-                .fillPhoneNumber("7077470510")
-                .setBirthDate("October", "1995", "10")
-                .setSubject("Hindi")
-                .selectHobby()
-                .uploadPicture("image.png")
-                .setAddress("Sports street 43")
-                .selectStateAndCity("Uttar Pradesh", "Lucknow")
-                .submitForm();
-
-        // Проверка, что поле Last Name не заполнено, и форма не отправляется
-        $("#lastName").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
+        formPage.checkFormNotSubmitted();
     }
 }
