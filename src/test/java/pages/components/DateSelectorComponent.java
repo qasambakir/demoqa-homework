872d84235af03cea9eaef3405469e1b03f6524b2
Samat <kosbarmak3@gmail.com>
2024-09-25 13:25:50 +0500

    commit DateSelectorComponent

--- src/test/java/pages/components/DateSelectorComponent.java
+++ src/test/java/pages/components/DateSelectorComponent.java
@@ -4,10 +4,8 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class DateSelectorComponent {
     public void setDate(String month, String year, String day) {
-        $(".react-datepicker__month-select").selectOption(month);
-
-        $(".react-datepicker__year-select").selectOption(year);
-
+        $(".react-datepicker__month-select").$("option[value='9']").click();
+        $(".react-datepicker__year-select").$("option[value='1995']").click();
         String daySelector = String.format(".react-datepicker__day--0%s:not(.react-datepicker__day--outside-month)", day);
         $(daySelector).click();
     }
