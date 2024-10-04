c3ca0bf0290468547ca26c35aa29a936268a67f3
Samat <kosbarmak3@gmail.com>
2024-10-04 11:48:36 +0500

    change aftereach

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,8 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -11,4 +13,10 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
     }
+
+    @AfterEach
+    void afterEach() {
+        Selenide.closeWebDriver();
+    }
 }
+
