add10e463163d78fca88606ec8ffdf88132960cd
SamBaker <qasambaker@yandex.ru>
2024-11-08 09:01:55 +0500

    change jenkins

--- src/test/java/tests/RegistrationAllureTests.java
+++ src/test/java/tests/RegistrationAllureTests.java
@@ -24,9 +24,7 @@ public class RegistrationAllureTests {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-//        Configuration.browser = "chrome";
         Configuration.timeout = 10000;
-//        Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
