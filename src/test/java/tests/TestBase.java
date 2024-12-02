813f30e3dfbf654842eef4dda6e689bb87f73f4c
SamBaker <qasambaker@yandex.ru>
2024-12-02 20:32:08 +0500

    change&added remote_url, remote_login

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -34,6 +35,7 @@ public class TestBase {
 
     @AfterEach
     void afterEach() {
+        Attach.addVideo();
         Selenide.closeWebDriver();
     }
 
