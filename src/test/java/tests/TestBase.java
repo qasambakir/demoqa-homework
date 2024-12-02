5f1a0926feb0d59e8c4d9a0676c244fd97a46062
SamBaker <qasambaker@yandex.ru>
2024-12-02 19:54:19 +0500

    change&added remote_url, remote_login

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -16,15 +16,16 @@ public class TestBase {
     static void setup() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://" + System.getProperty("login") + "@" + System.getProperty("remote");
-        Configuration.browserSize = System.getProperty("browserSize");
-        Configuration.browser = System.getProperty("browser");
-        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.remote = "https://" + System.getProperty("REMOTE_LOGIN") + "@" + System.getProperty("REMOTE_URL");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "126.0");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
-                "enableVideo", true
+                "enableVideo", true,
+                "videoName", System.getProperty("REMOTE_VIDEO_URL", "default_video.mp4")
         ));
         Configuration.browserCapabilities = capabilities;
 
